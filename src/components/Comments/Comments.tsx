4c70c1af3b0ce02a0841c2a019b31da68474d25e
JJ-33 <jonathan.jones@empired.com>
2022-03-21 10:56:02 +0800

    made the new comments section fade in based on auth

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,29 +1,29 @@
-import { Heading, VStack } from "@chakra-ui/react"
+import { Box, Fade, Heading, VStack } from "@chakra-ui/react"
 import Comment from "./Comment"
 import NewComment from "./NewComment"
 import { useContext } from "react"
 import { UserContext } from "../../context/UserContext"
 import { useComments } from "../../utils/hooks/comments"
 
-interface CommentsProps {
-    blogName:string
-}
 /**
  * To be used in mdx to add a comments section. Includes new comments for logged in users.
  */
-const Comments = (props:CommentsProps) => {
+const Comments = () => {
 
     const { authenticated } = useContext(UserContext)
     const {comments,addComment} = useComments()
 
-    return <VStack>
+    return <VStack width='full' maxW={'90ch'}>
                 <Heading as='h2'>
                     Comments
                 </Heading>
                 {comments.map((comment,i) => <Comment comment={comment} key={i}></Comment>)}
-                {authenticated && 
-                    <NewComment addComment={addComment} ></NewComment> 
-                }
+                <Fade  in={authenticated} style={{width:'100%'}}>
+                    <Box width={'full'}>
+                    <NewComment addComment={addComment} ></NewComment>
+                    </Box>
+                </Fade>
+                
             </VStack>
 }
 
